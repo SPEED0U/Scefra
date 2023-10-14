2bd76d863651d6e0b55d540248de015c246bc975
Steven M <34949863+SPEED0U@users.noreply.github.com>
2023-10-14 15:05:59 +0200

    Update install_fr.ps1

--- install_fr.ps1
+++ install_fr.ps1
@@ -3,8 +3,44 @@
 $localization1 = Join-Path $ScriptDir "LIVE"
 $localization2 = Join-Path $ScriptDir "PTU"
 
+# Fonction pour afficher le texte art ASCII
+function Show-ASCII-Art {
+    Write-Host @"
+                           *     .--.
+                                / /  `
+               +               | |
+                      '         \ \__,
+                  *          +   '--'  *
+                      +   /\
+         +              .'  '.   *
+                *      /======\      +
+                      ;:.  _   ;
+                      |:. (_)  |
+                      |:.  _   |
+            +         |:. (_)  |          *
+                      ;:.      ;
+                    .' \:.    / `.
+                   / .-'':._.'`-. \
+                   |/    /||\    \|
+             jgs _..--"""````"""--.._
+           _.-'``                    ``'-._
+         -'                                '-
+
+
+
+Contributeur:
+-SPEEDOU
+-Xety
+-viviengraffin
+-CNV_301/BOB
+
+
+"@
+}
+
 function Show-Menu {
     Clear-Host
+    Show-ASCII-Art
     Write-Host "Menu :"
     Write-Host "1. Traduire la version Live"
     Write-Host "2. Traduire la version PTU"
@@ -21,9 +57,11 @@ function Create-Shortcut {
     $shell = New-Object -ComObject WScript.Shell
     $shortcut = $shell.CreateShortcut($shortcutFile)
     $shortcut.TargetPath = $MyInvocation.MyCommand.Path
+    $shortcut.WorkingDirectory = $ScriptDir  # Spécifiez le répertoire de travail ici
     $shortcut.Save()
     Write-Host "Raccourci créé sur le bureau : $shortcutFile"
 }
+
 
 while ($true) {
     Show-Menu
@@ -31,23 +69,30 @@ while ($true) {
 
     switch ($choice) {
         '1' {
-            Write-Host "Vous avez choisi l'option 1."
+            Clear-Host
+            Write-Host "Traduction du Live."
 
+
+            # Vérification de la présence du Live
+            Write-Host "Vérification de la présence du Live"
             if (-not (Test-Path -Path $localization1 -PathType Container)) {
                 Write-Host "Je ne trouve pas le Live. Veuillez vérifier que vous m'avez placé dans le répertoire StarCitizen."
                 Read-Host "Appuyez sur Entrée pour continuer..."
             }
             else {
                 # Création du fichier user.cfg dans le live
+                Write-Host "Création du fichier user.cfg dans le live"
                 Set-Content -Path "$localization1\user.cfg" -Value 'g_language = french_(france)'
 
-                # Création des répertoires nécessaires dans le live
+                # Création des répertoires nécessaires dans le live"
+                Write-Host "Création des répertoires nécessaires dans le live"
                 $liveLocalizationDir = Join-Path $localization1 "data\Localization\french_(france)"
                 if (-not (Test-Path -Path $liveLocalizationDir -PathType Container)) {
                     New-Item -Path $liveLocalizationDir -ItemType Directory -Force
                 }
 
                 # Téléchargement et copie du fichier de traduction dans le live
+                Write-Host "Téléchargement et copie du fichier de traduction dans le live"
                 $url = "https://raw.githubusercontent.com/SPEED0U/StarCitizenTranslations/main/french_(france)/global.ini"
                 $outputPath = Join-Path $liveLocalizationDir "global.ini"
                 Invoke-WebRequest -Uri $url -OutFile $outputPath
@@ -57,7 +102,10 @@ while ($true) {
             }
         }
         '2' {
-            Write-Host "Vous avez choisi l'option 2."
+            Clear-Host
+            Write-Host "Traduction du PTU"
+            # Vérification de la présence du Live
+            Write-Host "Vérification de la présence du PTU"
 
             if (-not (Test-Path -Path $localization2 -PathType Container)) {
                 Write-Host "Je ne trouve pas la version PTU. Veuillez vérifier que vous m'avez placé dans le répertoire StarCitizen."
@@ -68,12 +116,14 @@ while ($true) {
                 Set-Content -Path "$localization2\user.cfg" -Value 'g_language = french_(france)'
 
                 # Création des répertoires nécessaires dans le ptu
+                Write-Host "Création des répertoires nécessaires dans le PTU"
                 $ptuLocalizationDir = Join-Path $localization2 "data\Localization\french_(france)"
                 if (-not (Test-Path -Path $ptuLocalizationDir -PathType Container)) {
                     New-Item -Path $ptuLocalizationDir -ItemType Directory -Force
                 }
 
                 # Téléchargement et copie du fichier de traduction dans le ptu
+                Write-Host "Téléchargement et copie du fichier de traduction dans le PTU"
                 $url = "https://raw.githubusercontent.com/SPEED0U/StarCitizenTranslations/main/french_(france)/global.ini"
                 $outputPath = Join-Path $ptuLocalizationDir "global.ini"
                 Invoke-WebRequest -Uri $url -OutFile $outputPath
@@ -83,6 +133,7 @@ while ($true) {
             }
         }
         '3' {
+            Clear-Host
             Write-Host "Vous avez choisi l'option 3."
 
             # Téléchargement du fichier de traduction depuis Internet
@@ -94,10 +145,14 @@ while ($true) {
             Read-Host "Appuyez sur Entrée pour continuer..."
         }
         '4' {
-            Write-Host "Vous avez choisi l'option 4."
+            Clear-Host
+            Write-Host "Ouverture de la page internet"
             Start-Process "https://speedou.sc.tasul.fr/"
+            Read-Host "Appuyez sur Entrée pour continuer..."
         }
         '5' {
+            Clear-Host
+            Write-Host "Création du raccourci sur le bureau : $shortcutFile"
             Create-Shortcut
             Read-Host "Appuyez sur Entrée pour continuer..."
         }
@@ -105,6 +160,7 @@ while ($true) {
             break
         }
         default {
+            Clear-Host
             Write-Host "Choix invalide. Veuillez choisir une option valide."
             Read-Host "Appuyez sur Entrée pour continuer..."
         }
@@ -112,152 +168,36 @@ while ($true) {
 }
 
 # SIG # Begin signature block
-# MIIbnQYJKoZIhvcNAQcCoIIbjjCCG4oCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# MIIF0wYJKoZIhvcNAQcCoIIFxDCCBcACAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUCcGdIM5xYUgeUQcqI9FW0SLs
-# NHWgghYTMIIDBjCCAe6gAwIBAgIQHtTmLk1VubFLfrslg97S6TANBgkqhkiG9w0B
-# AQsFADAbMRkwFwYDVQQDDBBBVEEgQXV0aGVudGljb2RlMB4XDTIzMTAxNDEwMjE1
-# NVoXDTI0MTAxNDEwNDE1NVowGzEZMBcGA1UEAwwQQVRBIEF1dGhlbnRpY29kZTCC
-# ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALN3OwEhXWTY/M55PHokEZDj
-# ui7fwr755LczNLi4nguAKUFiSplodtTYnM5LGTShXr7zTJy28/GJ+gfDgC/aJywM
-# /hHw5o1QvIWn1ntaEsHrHsEDiguPIpiEOWLc9l0PQeCdHw9MT98fXTedjDlFsFfR
-# boyImVseZg1JIg0vZUNotkzuertuks741GVncdMMXhsXjfnaDAM6PkgLZi1m3ia4
-# R4HLZ6sOUDLUDjNNH3kLklae+ZDHm3cgRNibxY3i2qMp0thb4KVQGrg3EUxmYzEr
-# WcPgdrIYL7ut2gVsyDueulH/0pwt2CkGNdsiHMU/+qweZz9gaRnI1d8PT9PqoyEC
-# AwEAAaNGMEQwDgYDVR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMB0G
-# A1UdDgQWBBQZz+hJoXCBbUpC/UpMvlDqm2xtsDANBgkqhkiG9w0BAQsFAAOCAQEA
-# EMOyIkofCc+EegRNB+Jm8Eayl0XKIp7W1PFj1NjdJofIr41zih3y4KVYbScpyxiU
-# XepkZEjknFgHs8MZ8po/5CzSjuB/qsCmdGt0aZsEQQRm30HrtOW0BEYP+mzer+PM
-# uumAMtwnHVBtJM/yRGd8AhcN/EoKTex47yEeXQO7VDxFaTr4j2ceWeS6R1pcmBoj
-# MTjXH51LCbnXiaSgURSyQrZb3Sxox3UdUTQ6CTAyCLrEKXnfQ4wXzBw0mfrJ5MPE
-# /xW2LlIXitupdy7aXqcUr1fVWbEdXHcJFEoJMIUE24rNDxjK/66/pk4er8JV2w/Z
-# uI9JhdYSvkn4QN9Uzmq3xTCCBY0wggR1oAMCAQICEA6bGI750C3n79tQ4ghAGFow
-# DQYJKoZIhvcNAQEMBQAwZTELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0
-# IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEkMCIGA1UEAxMbRGlnaUNl
-# cnQgQXNzdXJlZCBJRCBSb290IENBMB4XDTIyMDgwMTAwMDAwMFoXDTMxMTEwOTIz
-# NTk1OVowYjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcG
-# A1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEhMB8GA1UEAxMYRGlnaUNlcnQgVHJ1c3Rl
-# ZCBSb290IEc0MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAv+aQc2je
-# u+RdSjwwIjBpM+zCpyUuySE98orYWcLhKac9WKt2ms2uexuEDcQwH/MbpDgW61bG
-# l20dq7J58soR0uRf1gU8Ug9SH8aeFaV+vp+pVxZZVXKvaJNwwrK6dZlqczKU0RBE
-# EC7fgvMHhOZ0O21x4i0MG+4g1ckgHWMpLc7sXk7Ik/ghYZs06wXGXuxbGrzryc/N
-# rDRAX7F6Zu53yEioZldXn1RYjgwrt0+nMNlW7sp7XeOtyU9e5TXnMcvak17cjo+A
-# 2raRmECQecN4x7axxLVqGDgDEI3Y1DekLgV9iPWCPhCRcKtVgkEy19sEcypukQF8
-# IUzUvK4bA3VdeGbZOjFEmjNAvwjXWkmkwuapoGfdpCe8oU85tRFYF/ckXEaPZPfB
-# aYh2mHY9WV1CdoeJl2l6SPDgohIbZpp0yt5LHucOY67m1O+SkjqePdwA5EUlibaa
-# RBkrfsCUtNJhbesz2cXfSwQAzH0clcOP9yGyshG3u3/y1YxwLEFgqrFjGESVGnZi
-# fvaAsPvoZKYz0YkH4b235kOkGLimdwHhD5QMIR2yVCkliWzlDlJRR3S+Jqy2QXXe
-# eqxfjT/JvNNBERJb5RBQ6zHFynIWIgnffEx1P2PsIV/EIFFrb7GrhotPwtZFX50g
-# /KEexcCPorF+CiaZ9eRpL5gdLfXZqbId5RsCAwEAAaOCATowggE2MA8GA1UdEwEB
-# /wQFMAMBAf8wHQYDVR0OBBYEFOzX44LScV1kTN8uZz/nupiuHA9PMB8GA1UdIwQY
-# MBaAFEXroq/0ksuCMS1Ri6enIZ3zbcgPMA4GA1UdDwEB/wQEAwIBhjB5BggrBgEF
-# BQcBAQRtMGswJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBD
-# BggrBgEFBQcwAoY3aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0
-# QXNzdXJlZElEUm9vdENBLmNydDBFBgNVHR8EPjA8MDqgOKA2hjRodHRwOi8vY3Js
-# My5kaWdpY2VydC5jb20vRGlnaUNlcnRBc3N1cmVkSURSb290Q0EuY3JsMBEGA1Ud
-# IAQKMAgwBgYEVR0gADANBgkqhkiG9w0BAQwFAAOCAQEAcKC/Q1xV5zhfoKN0Gz22
-# Ftf3v1cHvZqsoYcs7IVeqRq7IviHGmlUIu2kiHdtvRoU9BNKei8ttzjv9P+Aufih
-# 9/Jy3iS8UgPITtAq3votVs/59PesMHqai7Je1M/RQ0SbQyHrlnKhSLSZy51PpwYD
-# E3cnRNTnf+hZqPC/Lwum6fI0POz3A8eHqNJMQBk1RmppVLC4oVaO7KTVPeix3P0c
-# 2PR3WlxUjG/voVA9/HYJaISfb8rbII01YBwCA8sgsKxYoA5AY8WYIsGyWfVVa88n
-# q2x2zm8jLfR+cWojayL/ErhULSd+2DrZ8LaHlv1b0VysGMNNn3O3AamfV6peKOK5
-# lDCCBq4wggSWoAMCAQICEAc2N7ckVHzYR6z9KGYqXlswDQYJKoZIhvcNAQELBQAw
-# YjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQ
-# d3d3LmRpZ2ljZXJ0LmNvbTEhMB8GA1UEAxMYRGlnaUNlcnQgVHJ1c3RlZCBSb290
-# IEc0MB4XDTIyMDMyMzAwMDAwMFoXDTM3MDMyMjIzNTk1OVowYzELMAkGA1UEBhMC
-# VVMxFzAVBgNVBAoTDkRpZ2lDZXJ0LCBJbmMuMTswOQYDVQQDEzJEaWdpQ2VydCBU
-# cnVzdGVkIEc0IFJTQTQwOTYgU0hBMjU2IFRpbWVTdGFtcGluZyBDQTCCAiIwDQYJ
-# KoZIhvcNAQEBBQADggIPADCCAgoCggIBAMaGNQZJs8E9cklRVcclA8TykTepl1Gh
-# 1tKD0Z5Mom2gsMyD+Vr2EaFEFUJfpIjzaPp985yJC3+dH54PMx9QEwsmc5Zt+Feo
-# An39Q7SE2hHxc7Gz7iuAhIoiGN/r2j3EF3+rGSs+QtxnjupRPfDWVtTnKC3r07G1
-# decfBmWNlCnT2exp39mQh0YAe9tEQYncfGpXevA3eZ9drMvohGS0UvJ2R/dhgxnd
-# X7RUCyFobjchu0CsX7LeSn3O9TkSZ+8OpWNs5KbFHc02DVzV5huowWR0QKfAcsW6
-# Th+xtVhNef7Xj3OTrCw54qVI1vCwMROpVymWJy71h6aPTnYVVSZwmCZ/oBpHIEPj
-# Q2OAe3VuJyWQmDo4EbP29p7mO1vsgd4iFNmCKseSv6De4z6ic/rnH1pslPJSlREr
-# WHRAKKtzQ87fSqEcazjFKfPKqpZzQmiftkaznTqj1QPgv/CiPMpC3BhIfxQ0z9JM
-# q++bPf4OuGQq+nUoJEHtQr8FnGZJUlD0UfM2SU2LINIsVzV5K6jzRWC8I41Y99xh
-# 3pP+OcD5sjClTNfpmEpYPtMDiP6zj9NeS3YSUZPJjAw7W4oiqMEmCPkUEBIDfV8j
-# u2TjY+Cm4T72wnSyPx4JduyrXUZ14mCjWAkBKAAOhFTuzuldyF4wEr1GnrXTdrnS
-# DmuZDNIztM2xAgMBAAGjggFdMIIBWTASBgNVHRMBAf8ECDAGAQH/AgEAMB0GA1Ud
-# DgQWBBS6FtltTYUvcyl2mi91jGogj57IbzAfBgNVHSMEGDAWgBTs1+OC0nFdZEzf
-# Lmc/57qYrhwPTzAOBgNVHQ8BAf8EBAMCAYYwEwYDVR0lBAwwCgYIKwYBBQUHAwgw
-# dwYIKwYBBQUHAQEEazBpMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2Vy
-# dC5jb20wQQYIKwYBBQUHMAKGNWh0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9E
-# aWdpQ2VydFRydXN0ZWRSb290RzQuY3J0MEMGA1UdHwQ8MDowOKA2oDSGMmh0dHA6
-# Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydFRydXN0ZWRSb290RzQuY3JsMCAG
-# A1UdIAQZMBcwCAYGZ4EMAQQCMAsGCWCGSAGG/WwHATANBgkqhkiG9w0BAQsFAAOC
-# AgEAfVmOwJO2b5ipRCIBfmbW2CFC4bAYLhBNE88wU86/GPvHUF3iSyn7cIoNqilp
-# /GnBzx0H6T5gyNgL5Vxb122H+oQgJTQxZ822EpZvxFBMYh0MCIKoFr2pVs8Vc40B
-# IiXOlWk/R3f7cnQU1/+rT4osequFzUNf7WC2qk+RZp4snuCKrOX9jLxkJodskr2d
-# fNBwCnzvqLx1T7pa96kQsl3p/yhUifDVinF2ZdrM8HKjI/rAJ4JErpknG6skHibB
-# t94q6/aesXmZgaNWhqsKRcnfxI2g55j7+6adcq/Ex8HBanHZxhOACcS2n82HhyS7
-# T6NJuXdmkfFynOlLAlKnN36TU6w7HQhJD5TNOXrd/yVjmScsPT9rp/Fmw0HNT7ZA
-# myEhQNC3EyTN3B14OuSereU0cZLXJmvkOHOrpgFPvT87eK1MrfvElXvtCl8zOYdB
-# eHo46Zzh3SP9HSjTx/no8Zhf+yvYfvJGnXUsHicsJttvFXseGYs2uJPU5vIXmVnK
-# cPA3v5gA3yAWTyf7YGcWoWa63VXAOimGsJigK+2VQbc61RWYMbRiCQ8KvYHZE/6/
-# pNHzV9m8BPqC3jLfBInwAM1dwvnQI38AC+R2AibZ8GV2QqYphwlHK+Z/GqSFD/yY
-# lvZVVCsfgPrA8g4r5db7qS9EFUrnEw4d2zc4GqEr9u3WfPwwggbCMIIEqqADAgEC
-# AhAFRK/zlJ0IOaa/2z9f5WEWMA0GCSqGSIb3DQEBCwUAMGMxCzAJBgNVBAYTAlVT
-# MRcwFQYDVQQKEw5EaWdpQ2VydCwgSW5jLjE7MDkGA1UEAxMyRGlnaUNlcnQgVHJ1
-# c3RlZCBHNCBSU0E0MDk2IFNIQTI1NiBUaW1lU3RhbXBpbmcgQ0EwHhcNMjMwNzE0
-# MDAwMDAwWhcNMzQxMDEzMjM1OTU5WjBIMQswCQYDVQQGEwJVUzEXMBUGA1UEChMO
-# RGlnaUNlcnQsIEluYy4xIDAeBgNVBAMTF0RpZ2lDZXJ0IFRpbWVzdGFtcCAyMDIz
-# MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAo1NFhx2DjlusPlSzI+DP
-# n9fl0uddoQ4J3C9Io5d6OyqcZ9xiFVjBqZMRp82qsmrdECmKHmJjadNYnDVxvzqX
-# 65RQjxwg6seaOy+WZuNp52n+W8PWKyAcwZeUtKVQgfLPywemMGjKg0La/H8JJJSk
-# ghraarrYO8pd3hkYhftF6g1hbJ3+cV7EBpo88MUueQ8bZlLjyNY+X9pD04T10Mf2
-# SC1eRXWWdf7dEKEbg8G45lKVtUfXeCk5a+B4WZfjRCtK1ZXO7wgX6oJkTf8j48qG
-# 7rSkIWRw69XloNpjsy7pBe6q9iT1HbybHLK3X9/w7nZ9MZllR1WdSiQvrCuXvp/k
-# /XtzPjLuUjT71Lvr1KAsNJvj3m5kGQc3AZEPHLVRzapMZoOIaGK7vEEbeBlt5NkP
-# 4FhB+9ixLOFRr7StFQYU6mIIE9NpHnxkTZ0P387RXoyqq1AVybPKvNfEO2hEo6U7
-# Qv1zfe7dCv95NBB+plwKWEwAPoVpdceDZNZ1zY8SdlalJPrXxGshuugfNJgvOupr
-# AbD3+yqG7HtSOKmYCaFxsmxxrz64b5bV4RAT/mFHCoz+8LbH1cfebCTwv0KCyqBx
-# PZySkwS0aXAnDU+3tTbRyV8IpHCj7ArxES5k4MsiK8rxKBMhSVF+BmbTO77665E4
-# 2FEHypS34lCh8zrTioPLQHsCAwEAAaOCAYswggGHMA4GA1UdDwEB/wQEAwIHgDAM
-# BgNVHRMBAf8EAjAAMBYGA1UdJQEB/wQMMAoGCCsGAQUFBwMIMCAGA1UdIAQZMBcw
-# CAYGZ4EMAQQCMAsGCWCGSAGG/WwHATAfBgNVHSMEGDAWgBS6FtltTYUvcyl2mi91
-# jGogj57IbzAdBgNVHQ4EFgQUpbbvE+fvzdBkodVWqWUxo97V40kwWgYDVR0fBFMw
-# UTBPoE2gS4ZJaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0VHJ1c3Rl
-# ZEc0UlNBNDA5NlNIQTI1NlRpbWVTdGFtcGluZ0NBLmNybDCBkAYIKwYBBQUHAQEE
-# gYMwgYAwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBYBggr
-# BgEFBQcwAoZMaHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0VHJ1
-# c3RlZEc0UlNBNDA5NlNIQTI1NlRpbWVTdGFtcGluZ0NBLmNydDANBgkqhkiG9w0B
-# AQsFAAOCAgEAgRrW3qCptZgXvHCNT4o8aJzYJf/LLOTN6l0ikuyMIgKpuM+AqNnn
-# 48XtJoKKcS8Y3U623mzX4WCcK+3tPUiOuGu6fF29wmE3aEl3o+uQqhLXJ4Xzjh6S
-# 2sJAOJ9dyKAuJXglnSoFeoQpmLZXeY/bJlYrsPOnvTcM2Jh2T1a5UsK2nTipgedt
-# QVyMadG5K8TGe8+c+njikxp2oml101DkRBK+IA2eqUTQ+OVJdwhaIcW0z5iVGlS6
-# ubzBaRm6zxbygzc0brBBJt3eWpdPM43UjXd9dUWhpVgmagNF3tlQtVCMr1a9TMXh
-# RsUo063nQwBw3syYnhmJA+rUkTfvTVLzyWAhxFZH7doRS4wyw4jmWOK22z75X7BC
-# 1o/jF5HRqsBV44a/rCcsQdCaM0qoNtS5cpZ+l3k4SF/Kwtw9Mt911jZnWon49qfH
-# 5U81PAC9vpwqbHkB3NpE5jreODsHXjlY9HxzMVWggBHLFAx+rrz+pOt5Zapo1iLK
-# O+uagjVXKBbLafIymrLS2Dq4sUaGa7oX/cR3bBVsrquvczroSUa31X/MtjjA2Owc
-# 9bahuEMs305MfR5ocMB3CtQC4Fxguyj/OOVSWtasFyIjTvTs0xf7UGv/B3cfcZdE
-# Qcm4RtNsMnxYL2dHZeUbc7aZ+WssBkbvQR7w8F/g29mtkIBEr4AQQYoxggT0MIIE
-# 8AIBATAvMBsxGTAXBgNVBAMMEEFUQSBBdXRoZW50aWNvZGUCEB7U5i5NVbmxS367
-# JYPe0ukwCQYFKw4DAhoFAKB4MBgGCisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJ
-# KoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQB
-# gjcCARUwIwYJKoZIhvcNAQkEMRYEFKjUtNziaNMZ6fzDZR6tYt0E2X5bMA0GCSqG
-# SIb3DQEBAQUABIIBADBp085JxzKGLkZSgmBQ/HeinFdyzTCI0zADeme+0mFTZkgx
-# C6oRBK/od7cRGy2uTKoY3M7W9yiVmL9QjMUCCN3lCWs4ecfLMIq3o3GDUteRcmDO
-# dev5XIbZJ7x3aLS7CiNq5L87EEjzRhH0Vx2vNBc/PAmeqtwbWGRdWYKDhzmNQ1Sf
-# 3YfsIXwUkgBqqArRQgTvdwtoAx+GI1pV1VXkQYBTPb0BEKaqVZnygah+puK6eOrh
-# 4ih9ymHRWk+dz6KawiGHPGlfzMu5icdiXW54rvy6KOEqrq7xmTB+xZqSnEcqNC+j
-# UFpL04dh+NG67kel4JCiUeFFBv0vmwXIztAh+EihggMgMIIDHAYJKoZIhvcNAQkG
-# MYIDDTCCAwkCAQEwdzBjMQswCQYDVQQGEwJVUzEXMBUGA1UEChMORGlnaUNlcnQs
-# IEluYy4xOzA5BgNVBAMTMkRpZ2lDZXJ0IFRydXN0ZWQgRzQgUlNBNDA5NiBTSEEy
-# NTYgVGltZVN0YW1waW5nIENBAhAFRK/zlJ0IOaa/2z9f5WEWMA0GCWCGSAFlAwQC
-# AQUAoGkwGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcN
-# MjMxMDE0MTAzNDM3WjAvBgkqhkiG9w0BCQQxIgQgZMfxgBZ92tQALIVXjejAF2s1
-# qzSUYkuL2hgl3qT+qxAwDQYJKoZIhvcNAQEBBQAEggIAZc5XoRhlYIy8sUYFF6DR
-# E5np1lzeIME6IN+tm80K8x1b5bG4ry0Oo/9mk3vOfiYNl83692pfmTflpLISDL7q
-# RUD/qeORXQ8mVisRIF1wTqqCiL/ykAwNYXpKWDEmaxZAGml+s2w2DgZBP8LpHMF9
-# XeIrK9w8onARrfpo3d6QtPKiNaWiQCcw8xqUpQ7YwuQ9dLnx+PLxvJtfLQvOAOxL
-# N4J4wpd5qSjx01/vdAgDrwnu4cA5y+xAP0kNFWSPOluC1FsumQ2Lf67BBJoYYjb2
-# Ij1gtVl8tuoPnHDazndouELBJLmGbmbWWpUkhWZh/luU7IR6dI2QF++B+rV9SKAE
-# HFUVIcxx2xAoQYEvHLt5xAWJbvdi8Xa/ogZeltsDw97Lj0udkxQr76ecFhiLLQ0B
-# h81d3uEK8CMcxf0gu1jL/qjYcG59rsAwQyECEhvUMaTrOGR90aJIdX6aNChPgZcr
-# pVc3Ve+TX80QDGTac3riRvN8M6TuDSprGmk/YM6ZSVrv9fsN8irvfCoZ2inhu+Co
-# 0jD2A3LEmKQVkmo2tietCnEli0y0PNsF6SU6zquCNHeNCut83SLuR2Jx83oAm9V1
-# hJ8jsuTd1a6nuL8DJVUvpsUr9u4Q5VS0G4WO+oTls9Int8qapk7aiepF8BxFpfu1
-# pEO8jzy7aGp0Ho7DiP54Xq0=
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUib/N5znfXbXXl4fBnE97PV9t
+# ONCgggNMMIIDSDCCAjCgAwIBAgIQaYOAw5aCEKJMOMPCq5dBLzANBgkqhkiG9w0B
+# AQsFADA8MRAwDgYDVQQDDAdET1VTUEVFMSgwJgYJKoZIhvcNAQkBFhlzcGVlZG91
+# LnRtLnByb2RAZ21haWwuY29tMB4XDTIzMTAxNDEyNDQxOFoXDTM4MTIzMTIyMDAw
+# MFowPDEQMA4GA1UEAwwHRE9VU1BFRTEoMCYGCSqGSIb3DQEJARYZc3BlZWRvdS50
+# bS5wcm9kQGdtYWlsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
+# AN+f2OaAAr+NG/EIHMfdM7oGSM30SA2EkBQkHXwdUxPxqaQm8OKyl24czBpUk+ng
+# QRhICPQCRe7KILfFmX0U2WqDh10J/onALDx3dZ27iVRl1hby1KO9Bx4dVYhSrdrT
+# Nzmkp9heBGDv/GZVst9JV9OkRlc5MLL7tF6NdBtqJ3rS/ue5IAOay87uVXwPuqcv
+# tgiP6fyL8dgthyCE1ANu95fQO4pnRb2dnybtem6ck2y/92KotEZd6M9/5/21GSoa
+# ShTMNToAKlMY8VXAln0Mm80HizcFpOTnrKRvl56Bhzt2WKsbjpo7iBb71ziX5sKY
+# TVzuIGdTStHMfoNpoHmM95ECAwEAAaNGMEQwDgYDVR0PAQH/BAQDAgWgMBMGA1Ud
+# JQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBTpnNv0nWvsRVSlFSWhBw1dPwR1ZTAN
+# BgkqhkiG9w0BAQsFAAOCAQEAQ1VoAFn4kYMQZrkKftgzBsV7PXvWM4emSQp/eB4m
+# KmkIh/aWfuNJNdsOMTdBpWQik7vto/ohqa8jZokQWzDPQX6nYkCtGjknRvMXXmP2
+# vawk9GdGxASpIJesulSBDxKoSno7J2CBxRhjFH+ORCD1H1hcRj9cja9Ht0JUwMcV
+# tOH8t/I9kFxYzGA4u09sR+a5KwfsL2fNlAOwPI+yerEKB3asNR5TQ1sMrdGtyYcu
+# dw/MFkvifPaZ/GiPYL+e4ikf31o3MeAb3P2TvJgtNtTTdDrepNzSVEdhJA0Bw650
+# ZBXLPy8LYaNJf7dTav1q4UlZ+07sei7seyMBE0xtGhl3tDGCAfEwggHtAgEBMFAw
+# PDEQMA4GA1UEAwwHRE9VU1BFRTEoMCYGCSqGSIb3DQEJARYZc3BlZWRvdS50bS5w
+# cm9kQGdtYWlsLmNvbQIQaYOAw5aCEKJMOMPCq5dBLzAJBgUrDgMCGgUAoHgwGAYK
+# KwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIB
+# BDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQU
+# zvVCVQc22Mj9nMna8gPvOuyNdEYwDQYJKoZIhvcNAQEBBQAEggEAYO3jaAjL/agl
+# SNcxoEeFViT26otUo3XTyL1RNU1pL/JzbbKovqihOHx1g9879yCBwgcW1EWBp5P+
+# hyPHtUMRK0xM3DhHDVQcZQRvv/rze5wnLGdSPBRzkKucroTF35zAHR4hklCBVBFm
+# qpV6bbraZ12jN6fOw53jl6sv+FYXtyoLDr3IrxzxZ+OAzo/MhE196s+U15DHLj+u
+# Q+Skse01no9XpGpDXjGHEPrMqCcgevHFRbSWJFdUcVtsCt4PgBnPplNYIj50iFSy
+# YfHAOcxgSWhRpUrvfmBlBEOoWno+IQH1HJXYIPDnx5W55BOgVAUAov9MqDGK2kl9
+# wUCrv35E6Q==
 # SIG # End signature block
