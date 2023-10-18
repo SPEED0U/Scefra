a7dd7ae3acc6bf4e956e04310f1f74db2c3ee822
Steven M <34949863+SPEED0U@users.noreply.github.com>
2023-10-18 17:54:31 +0200

    Fixed script not saving with BOM

--- install_fr.ps1
+++ install_fr.ps1
@@ -26,15 +26,11 @@ function Show-ASCII-Art {
            _.-'``                    ``'-._
          -'                                '-
 
-
-
 Contributeur:
 -SPEEDOU
 -Xety
 -viviengraffin
 -CNV_301/BOB
-
-
 "@
 }
 
@@ -62,7 +58,6 @@ function Create-Shortcut {
     Write-Host "Raccourci créé sur le bureau : $shortcutFile"
 }
 
-
 while ($true) {
     Show-Menu
     $choice = Read-Host "Choisissez une option"
@@ -71,7 +66,6 @@ while ($true) {
         '1' {
             Clear-Host
             Write-Host "Traduction du Live."
-
 
             # Vérification de la présence du Live
             Write-Host "Vérification de la présence du Live"
@@ -96,7 +90,13 @@ while ($true) {
                 Write-Host "Téléchargement et copie du fichier de traduction dans le live"
                 $url = "https://raw.githubusercontent.com/SPEED0U/StarCitizenTranslations/main/french_(france)/global.ini"
                 $outputPath = Join-Path $liveLocalizationDir "global.ini"
-                Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+                # Télécharge le contenu du fichier
+                $response = Invoke-WebRequest -Uri $url
+
+                # Assurez-vous que le contenu est en UTF-8 avec BOM et CRLF
+                $utf8Encoding = New-Object System.Text.UTF8Encoding $true
+                [System.IO.File]::WriteAllText($outputPath, $response.Content, $utf8Encoding)
 
                 Write-Host "Installation du dernier fichier de traduction dans le live, terminé."
                 Read-Host "Appuyez sur Entrée pour continuer..."
@@ -115,7 +115,7 @@ while ($true) {
             else {
                 # Création du fichier user.cfg dans le ptu
                 Set-Content -Path "$localization2\user.cfg" -Value 'g_language = french_(france)'
-                Add-Content -Path "$localization1\user.cfg" -Value 'g_languageAudio = english'
+                Add-Content -Path "$localization2\user.cfg" -Value 'g_languageAudio = english'
 
                 # Création des répertoires nécessaires dans le ptu
                 Write-Host "Création des répertoires nécessaires dans le PTU"
@@ -124,11 +124,17 @@ while ($true) {
                     New-Item -Path $ptuLocalizationDir -ItemType Directory -Force
                 }
 
-                # Téléchargement et copie du fichier de traduction dans le ptu
+                # Téléchargement et copie du fichier de traduction dans le PTU
                 Write-Host "Téléchargement et copie du fichier de traduction dans le PTU"
                 $url = "https://raw.githubusercontent.com/SPEED0U/StarCitizenTranslations/main/french_(france)/global.ini"
                 $outputPath = Join-Path $ptuLocalizationDir "global.ini"
-                Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+                # Télécharge le contenu du fichier
+                $response = Invoke-WebRequest -Uri $url
+
+                # Assurez-vous que le contenu est en UTF-8 avec BOM et CRLF
+                $utf8Encoding = New-Object System.Text.UTF8Encoding $true
+                [System.IO.File]::WriteAllText($outputPath, $response.Content, $utf8Encoding)
 
                 Write-Host "Installation du dernier fichier de traduction dans le PTU, terminé."
                 Read-Host "Appuyez sur Entrée pour continuer..."
@@ -137,19 +143,31 @@ while ($true) {
         '3' {
             Clear-Host
             Write-Host "Vous avez choisi l'option 3."
-
+        
             # Téléchargement du fichier de traduction depuis Internet
             $url = "https://raw.githubusercontent.com/SPEED0U/StarCitizenTranslations/main/french_(france)/global.ini"
             $outputPath = Join-Path $ScriptDir "global.ini"
-            Invoke-WebRequest -Uri $url -OutFile $outputPath
-
-            Write-Host "Récupération du dernier fichier de traduction, terminé."
+        
+            # Télécharge le contenu du fichier
+            $response = Invoke-WebRequest -Uri $url
+        
+            # Crée un StreamWriter avec l'encodage UTF-8 et BOM pour écrire le fichier
+            $utf8EncodingWithBOM = New-Object System.Text.UTF8Encoding $true
+            $writer = [System.IO.StreamWriter]::new($outputPath, $false, $utf8EncodingWithBOM)
+        
+            # Écrit le contenu dans le fichier
+            $writer.Write($response.Content)
+        
+            # Ferme le StreamWriter
+            $writer.Close()
+        
+            Write-Host "Récupération du dernier fichier de traduction."
             Read-Host "Appuyez sur Entrée pour continuer..."
         }
         '4' {
             Clear-Host
             Write-Host "Ouverture de la page internet"
-            Start-Process "https://speedou.sc.tasul.fr/"
+            Start-Process "https://tradsc.nightriderz.world/fr/"
             Read-Host "Appuyez sur Entrée pour continuer..."
         }
         '5' {
@@ -168,38 +186,3 @@ while ($true) {
         }
     }
 }
-
-# SIG # Begin signature block
-# MIIF0wYJKoZIhvcNAQcCoIIFxDCCBcACAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUib/N5znfXbXXl4fBnE97PV9t
-# ONCgggNMMIIDSDCCAjCgAwIBAgIQaYOAw5aCEKJMOMPCq5dBLzANBgkqhkiG9w0B
-# AQsFADA8MRAwDgYDVQQDDAdET1VTUEVFMSgwJgYJKoZIhvcNAQkBFhlzcGVlZG91
-# LnRtLnByb2RAZ21haWwuY29tMB4XDTIzMTAxNDEyNDQxOFoXDTM4MTIzMTIyMDAw
-# MFowPDEQMA4GA1UEAwwHRE9VU1BFRTEoMCYGCSqGSIb3DQEJARYZc3BlZWRvdS50
-# bS5wcm9kQGdtYWlsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
-# AN+f2OaAAr+NG/EIHMfdM7oGSM30SA2EkBQkHXwdUxPxqaQm8OKyl24czBpUk+ng
-# QRhICPQCRe7KILfFmX0U2WqDh10J/onALDx3dZ27iVRl1hby1KO9Bx4dVYhSrdrT
-# Nzmkp9heBGDv/GZVst9JV9OkRlc5MLL7tF6NdBtqJ3rS/ue5IAOay87uVXwPuqcv
-# tgiP6fyL8dgthyCE1ANu95fQO4pnRb2dnybtem6ck2y/92KotEZd6M9/5/21GSoa
-# ShTMNToAKlMY8VXAln0Mm80HizcFpOTnrKRvl56Bhzt2WKsbjpo7iBb71ziX5sKY
-# TVzuIGdTStHMfoNpoHmM95ECAwEAAaNGMEQwDgYDVR0PAQH/BAQDAgWgMBMGA1Ud
-# JQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBTpnNv0nWvsRVSlFSWhBw1dPwR1ZTAN
-# BgkqhkiG9w0BAQsFAAOCAQEAQ1VoAFn4kYMQZrkKftgzBsV7PXvWM4emSQp/eB4m
-# KmkIh/aWfuNJNdsOMTdBpWQik7vto/ohqa8jZokQWzDPQX6nYkCtGjknRvMXXmP2
-# vawk9GdGxASpIJesulSBDxKoSno7J2CBxRhjFH+ORCD1H1hcRj9cja9Ht0JUwMcV
-# tOH8t/I9kFxYzGA4u09sR+a5KwfsL2fNlAOwPI+yerEKB3asNR5TQ1sMrdGtyYcu
-# dw/MFkvifPaZ/GiPYL+e4ikf31o3MeAb3P2TvJgtNtTTdDrepNzSVEdhJA0Bw650
-# ZBXLPy8LYaNJf7dTav1q4UlZ+07sei7seyMBE0xtGhl3tDGCAfEwggHtAgEBMFAw
-# PDEQMA4GA1UEAwwHRE9VU1BFRTEoMCYGCSqGSIb3DQEJARYZc3BlZWRvdS50bS5w
-# cm9kQGdtYWlsLmNvbQIQaYOAw5aCEKJMOMPCq5dBLzAJBgUrDgMCGgUAoHgwGAYK
-# KwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIB
-# BDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQU
-# zvVCVQc22Mj9nMna8gPvOuyNdEYwDQYJKoZIhvcNAQEBBQAEggEAYO3jaAjL/agl
-# SNcxoEeFViT26otUo3XTyL1RNU1pL/JzbbKovqihOHx1g9879yCBwgcW1EWBp5P+
-# hyPHtUMRK0xM3DhHDVQcZQRvv/rze5wnLGdSPBRzkKucroTF35zAHR4hklCBVBFm
-# qpV6bbraZ12jN6fOw53jl6sv+FYXtyoLDr3IrxzxZ+OAzo/MhE196s+U15DHLj+u
-# Q+Skse01no9XpGpDXjGHEPrMqCcgevHFRbSWJFdUcVtsCt4PgBnPplNYIj50iFSy
-# YfHAOcxgSWhRpUrvfmBlBEOoWno+IQH1HJXYIPDnx5W55BOgVAUAov9MqDGK2kl9
-# wUCrv35E6Q==
-# SIG # End signature block
